c2f031aef4c01041b2a8504d4499be03ecdbd442
Christopher Acosta <chriz@live.dk>
2020-10-11 14:05:30 +0200

    bubble up/down. todo: tests?

--- MinHeap/MinHeap.hpp
+++ MinHeap/MinHeap.hpp
@@ -1,6 +1,8 @@
 #ifndef MIN_HEAP_H
 #define MIN_HEAP_H
 
+#include <iostream>
+
 class MinHeap {
   int* heap;
   int size;
@@ -17,6 +19,19 @@ public:
   void insert(int);
   bool empty();
   bool full();
+
+  friend std::ostream& operator<<(std::ostream& out, const MinHeap& mh) {
+    if (mh.size == 0) {
+      out << "[]";
+      return out;
+    }
+    out << "[";
+    out << mh.heap[1];
+    for (int i = 2; i <= mh.size; i++)
+      out << ", " << mh.heap[i];
+    out << "]";
+    return out;
+  }
 };
 
 MinHeap::MinHeap(int cap): capacity(cap), size(0), heap(new int[cap]) { }
@@ -36,19 +51,20 @@ int MinHeap::right_child(int i) {
 void MinHeap::insert(int n) {
   if (full())
     throw std::runtime_error("Heap is full");
+  size++;
   heap[size] = n;
-  bubble_up(size++);
+  bubble_up(size);
 }
 
 int MinHeap::min() {
   if (empty())
-    throw std::runtime_error("Heap is full");
+    throw std::runtime_error("Heap is empty");
   return heap[0];
 }
 
 int MinHeap::extract_min() {
   if (empty())
-    throw std::runtime_error("Heap is full");
+    throw std::runtime_error("Heap is empty");
   int min = heap[0];
   heap[0] = heap[--size];
   bubble_down(0);
@@ -64,11 +80,39 @@ bool MinHeap::full() {
 }
 
 void MinHeap::bubble_up(int i) {
-  // todo
+  if (i == 1)
+    return;
+
+  int p = parent(i);
+  int val_i = heap[i];
+  int val_p = heap[p];
+  
+  if (val_i < val_p) {
+    heap[p] = val_i;
+    heap[i] = val_p;
+    bubble_up(p);
+  }
 }
 
 void MinHeap::bubble_down(int i) {
-  // todo
+  int l = left_child(i);
+  if (l > size)
+    return;
+
+  int val_i = heap[i];
+  int val_l = heap[l];
+  int r = right_child(i);
+  int val_r = heap[r];
+  if (r > size && val_l < val_i) {
+    heap[i] = val_l;
+    heap[l] = val_i;
+  } else if (val_i > val_l || val_i > val_r) {
+    int val_r = heap[r];
+    heap[i] = std::min(val_l, val_r);
+    int min_ix = val_l < val_r ? l : r;
+    heap[min_ix] = val_i;
+    bubble_down(min_ix);
+  }
 }
 
 
--- MinHeap/main.cpp
+++ MinHeap/main.cpp
@@ -4,5 +4,35 @@
 using namespace std;
 
 int main() {
-  cout << "hi\n";
+  MinHeap heap = MinHeap(16);
+  cout << "Empty min heap:\n";
+  cout << heap << "\n";
+
+  cout << "Inserting 7:\n";
+  heap.insert(7);
+  cout << heap << "\n";
+
+  cout << "Inserting 9:\n";
+  heap.insert(9);
+  cout << heap << "\n";
+
+  cout << "Inserting 3:\n";
+  heap.insert(3);
+  cout << heap << "\n";
+
+  cout << "Inserting 5:\n";
+  heap.insert(5);
+  cout << heap << "\n";
+
+  cout << "Inserting 10:\n";
+  heap.insert(10);
+  cout << heap << "\n";
+
+  cout << "Inserting 2:\n";
+  heap.insert(2);
+  cout << heap << "\n";
+
+  cout << "Inserting 6:\n";
+  heap.insert(6);
+  cout << heap << "\n";
 }
